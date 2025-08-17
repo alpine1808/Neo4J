07e75f4ee8b66fba0ba352666908d092a7d89db9
alpine1808 <panzo2023@gmail.com>
2025-08-17 09:41:09 +0700

    Update

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
-NEO4J_URI = "neo4j+s://55971f28.databases.neo4j.io"
-NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "ZCCCayMUZPXXkNluxSeAZZxnrUstAT_co5loD2qkuRI"
+from dotenv import load_dotenv
+
+load_dotenv()
+
+NEO4J_URI = os.getenv("NEO4J_URI")
+NEO4J_USER = os.getenv("NEO4J_USER")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
 LOG_FILE = os.getenv("LOG_FILE", "update.log")
 BATCH_SIZE = int(os.getenv("BATCH_SIZE", "500"))
